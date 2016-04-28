51a41464d5dcfcb171b6c374f14d44b39f247916
yaoyao1992 <tangzhijie115@sina.com>
2016-04-28 16:37:35 +0800

    update on 4/27

--- www/echo.js
+++ www/echo.js
@@ -1,10 +1,10 @@
 var exec = require("cordova/exec");
-
+var echoExport={};
 
 
 echoExport.getMessage=function(successCallback,errorCallback,options){
 
-	exec(successCallback,errorCallback,"echo","getMessage",options);
+	exec(successCallback,errorCallback,"Echo","echo",options);
 }
 
 module.exports=echoExport;
