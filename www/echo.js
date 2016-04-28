8397475e7cce2ce366938e6c39c78b057ac9486c
yaoyao1992 <tangzhijie115@sina.com>
2016-04-28 16:29:39 +0800

    fiveth

--- www/echo.js
+++ www/echo.js
@@ -4,7 +4,7 @@ var exec = require("cordova/exec");
 
 echoExport.getMessage=function(successCallback,errorCallback,options){
 
-	cordova.exec(successCallback,errorCallback,"echo","getMessage",options);
+	exec(successCallback,errorCallback,"echo","getMessage",options);
 }
 
 module.exports=echoExport;
